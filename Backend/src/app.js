d12cecee35531e23fca931596fe994a592bef255
Rashi <rashishaha0610@gmail.com>
2026-04-10 16:16:43 +0530

    cors issue fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ import interviewrouter from "./routes/interview.routes.js"
 export const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://your-frontend.vercel.app"],
     credentials:true
 }))
 app.use(express.json())
